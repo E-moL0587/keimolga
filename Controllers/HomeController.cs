c3186dd2838ddd42dac8716086373d82eee84301
E-moL0587 <kai.pika.025@outlook.com>
2024-06-12 20:21:51 +0900

    update

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -23,11 +23,6 @@ public class HomeController : Controller
         return View();
     }
 
-    public IActionResult Event()
-    {
-        return View();
-    }
-
     public IActionResult Contact()
     {
         return View();
--- obj/Debug/net8.0/keimolga.AssemblyInfo.cs
+++ obj/Debug/net8.0/keimolga.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("keimolga")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4ba1710a43aff61ebb08a5774260b3cbfe812c90")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51b3de9c414c31c8f5aa0792b97f9261c7e9fd22")]
 [assembly: System.Reflection.AssemblyProductAttribute("keimolga")]
 [assembly: System.Reflection.AssemblyTitleAttribute("keimolga")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
