6617142b2b779aee727a6b97010d61700573fcf1
E-moL0587 <kai.pika.025@outlook.com>
2024-06-13 15:00:09 +0900

    update

--- obj/Release/net8.0/keimolga.AssemblyInfo.cs
+++ obj/Release/net8.0/keimolga.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("keimolga")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+38c6e6a747f2bb123b1465d18cb86c69b801d49f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1066f403ba734032497db9534afa70234530366d")]
 [assembly: System.Reflection.AssemblyProductAttribute("keimolga")]
 [assembly: System.Reflection.AssemblyTitleAttribute("keimolga")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
